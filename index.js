a42f7e950c096078c334b9b7e36c6dc07ae10ad0
Alex Kurkov <alexkourkov@yandex.ru>
2020-07-14 15:01:22 +0500

    feat: add cards thru js-template

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+        alt: 'фотография гор Архыза',
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+        alt: 'фотография пейзажа Челябинской области',
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+        alt: 'фотография городского пейзажа Иваново',
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+        alt: 'фотография природы Камчатки',
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+        alt: 'фотография железной дороги в лесу в Холмогорах',
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+        alt: 'фотография скал и скованного льдом Байкала',
+    }
+];
 const content = document.querySelector('.content');
 const profile = content.querySelector('.profile');
 const name = profile.querySelector('.profile__title');
@@ -9,6 +41,8 @@ const inputAbout = popupForm.querySelector('.form__input_txt_about');
 const btnEditProfile = profile.querySelector('.profile__edit-button');
 const btnClosePopup = popup.querySelector('.popup-form__close-btn');
 const form = popup.querySelector('.form');
+const cardTemplate = document.querySelector('#places__card').content;
+const placesContainer = content.querySelector('.places__container');
 
 const addInputName = () => {
     inputName.value = name.textContent;
@@ -31,6 +65,19 @@ const submitForm = (event) => {
     about.textContent = inputAbout.value;
     togglePopup();
 };
+
+initialCards.forEach(({name, link = 'https://clck.ru/PeeMN', alt = 'Фотография места'}) => {
+    const placesCard = cardTemplate.cloneNode(true);
+    
+    placesCard.querySelector('.places__card-image').src = link;
+    placesCard.querySelector('.places__card-image').alt = alt;
+    placesCard.querySelector('.places__card-title').textContent = name;
+    
+    placesCard.querySelector('.places__like-button').addEventListener('click', () => {});
+    placesCard.querySelector('.places__trash-btn').addEventListener('click', () => {});
+
+    placesContainer.prepend(placesCard);
+})
 
 btnEditProfile.addEventListener('click', openPopup);
 btnClosePopup.addEventListener('click', togglePopup);
