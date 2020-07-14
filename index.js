72db72c95a34907534d36337af95bc51e58be8d5
Alex Kurkov <alexkourkov@yandex.ru>
2020-07-14 18:34:58 +0500

    feat: add card-add popup

--- index.js
+++ index.js
@@ -36,37 +36,59 @@ const name = profile.querySelector('.profile__title');
 const about = profile.querySelector('.profile__profession');
 const popup = document.querySelector('.popup');
 const popupForm = popup.querySelector('.popup-form');
-const inputName = popupForm.querySelector('.form__input_txt_name');
-const inputAbout = popupForm.querySelector('.form__input_txt_about');
 const btnEditProfile = profile.querySelector('.profile__edit-button');
-const btnClosePopup = popup.querySelector('.popup-form__close-btn');
-const form = popup.querySelector('.form');
+const btnAddCard = profile.querySelector('.profile__add-button');
+const formProfileEdit = popup.querySelector('.popup-form_type_profile-edit');
+const inputName = formProfileEdit.querySelector('.form__input_txt_name');
+const inputAbout = formProfileEdit.querySelector('.form__input_txt_about');
+const formCardEdit = popup.querySelector('.popup-form_type_card-edit');
+const inputTitle = formCardEdit.querySelector('.form__input_txt_title');
+const inputLink = formCardEdit.querySelector('.form__input_txt_link');
 const cardTemplate = document.querySelector('#places__card').content;
 const placesContainer = content.querySelector('.places__container');
 
+const toggleElementVisibility = (element) => {
+    element.classList.toggle('display_is-visible');
+};
+const togglePopup = () => {
+    popup.classList.toggle('popup_opened');
+};
+const closePopup = (evt) => {
+    togglePopup();
+    const buttonParent = evt.target.parentElement;
+    toggleElementVisibility(buttonParent);
+};
+
+const openProfilePopup = () => {
+    inputName.value = name.textContent;
+    inputAbout.value = about.textContent;
+    togglePopup();
+    toggleElementVisibility(formProfileEdit);
+
+    const btnClosePopup = formProfileEdit.querySelector('.popup-form__close-btn');
+    btnClosePopup.addEventListener('click', closePopup);
+};
 const addInputName = () => {
     inputName.value = name.textContent;
 };
 const addInputAbout = () => {
     inputAbout.value = about.textContent;
 };
-const togglePopup = () => {
-    popup.classList.toggle('popup_opened');
-};
-const openPopup = () => {
-    inputName.value = name.textContent;
-    inputAbout.value = about.textContent;
-    togglePopup();
-};
-const submitForm = (event) => {
-    console.log('sdlkjsaldkvc');
-    event.preventDefault();
+const submitProfileForm = (evt) => {
+    evt.preventDefault();
     name.textContent = inputName.value;
     about.textContent = inputAbout.value;
-    togglePopup();
+    closePopup(evt);
 };
 
-initialCards.forEach(({name, link = 'https://clck.ru/PeeMN', alt = 'Фотография места'}) => {
+const addCardPopup = () => {
+    togglePopup();
+    toggleElementVisibility(formCardEdit);
+    
+    const btnClosePopup = formCardEdit.querySelector('.popup-form__close-btn');
+    btnClosePopup.addEventListener('click', closePopup);
+};
+const makeCard = ({name = 'Место N', link = 'https://clck.ru/PeeMN', alt = 'Фотография места'}) => {
     const placesCard = cardTemplate.cloneNode(true);
     
     placesCard.querySelector('.places__card-image').src = link;
@@ -77,8 +99,26 @@ initialCards.forEach(({name, link = 'https://clck.ru/PeeMN', alt = 'Фотогр
     placesCard.querySelector('.places__trash-btn').addEventListener('click', () => {});
 
     placesContainer.prepend(placesCard);
-})
+};
 
-btnEditProfile.addEventListener('click', openPopup);
-btnClosePopup.addEventListener('click', togglePopup);
-form.addEventListener("submit", submitForm);
+const isValidUrl = (url) => {
+    const u = /http(s?):\/\/[-\w\.]{3,}\.[A-Za-z]{2,3}/;
+    return u.test(url);
+};
+
+const submitNewCard = (evt) => {
+    evt.preventDefault();
+    const newPlace = { 
+        name: inputTitle.value,
+        link: isValidUrl(inputLink.value) ? inputLink.value : undefined,
+    };
+    makeCard (newPlace);
+    closePopup(evt);
+};
+
+initialCards.forEach(makeCard);
+
+btnEditProfile.addEventListener('click', openProfilePopup);
+btnAddCard.addEventListener('click', addCardPopup);
+formProfileEdit.addEventListener("submit", submitProfileForm);
+formCardEdit.addEventListener('submit', submitNewCard);
